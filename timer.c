f61e8bfa485c7e55baaa5e4456b620b4c77cce32
Afoxys <38985979+Afoxys@users.noreply.github.com>
2018-10-13 15:12:26 +0100

    Add files via upload

--- timer.c
+++ timer.c
@@ -41,15 +41,15 @@ int (timer_get_conf)(uint8_t timer, uint8_t *st) {
 
 	byte = (byte | TIMER_RB_CMD | TIMER_RB_COUNT_); //Defining byte value
 
-	if (sys_outb(TIMER_CTRL, byte) != 0) { //Checking for errors in the comand sys_outb 
-		printf("Function error");
+	if (sys_outb(TIMER_CTRL, byte) != 0) { //Checking for errors, using the control clock
+		printf("Function error/n");
 		return 1; //Must return non-zero value if the function doesn't work
 
 	}
 
 
 
-  switch(timer){  //Switch-case function to define the behaviour for the diffrent clocks, we will read and save the clock information in the byte variable
+  switch(timer){  //Switch-case function to define the behaviour for the diffrent clocks, we will save the clock information in the byte variable
 
     case 0:
 			sys_inb(TIMER_0, &byte);
@@ -64,7 +64,7 @@ int (timer_get_conf)(uint8_t timer, uint8_t *st) {
 			break;
   }
 
-  *st = (uint8_t)byte;
+  *st = (uint8_t)byte; //Saving the information contained in the byte variable to the memory
 
   return 0;
 }
@@ -73,32 +73,36 @@ int (timer_get_conf)(uint8_t timer, uint8_t *st) {
 int (timer_display_conf)(uint8_t timer, uint8_t st, enum timer_status_field field) {
 
 	union timer_status_field_val conf;
+	uint8_t byte;
 	
 	switch(field){
 		case all:
-			conf = st;
+			conf.byte = st;
 			break;
 		case initial:
-			switch(in_mode){
-				case INVAL_val:
-					break;
-				case LSB_only:
-					break;
-				case MSB_only:	
-					break;
-				case MSB_after_LSB:
-					break;
+			byte = (st | BIT(5) | BIT(4));
+			switch (byte) {
+				case 01:
+				  conf.in_mode = LSB_only;
+				  break;
+				case 10:
+					conf.in_mode = MSB_only;
+				  break;
+				case 11:
+				   conf.in_mode = MSB_after_LSB;
+				  break;
 			}
 		case mode:
-			conf = (st | BIT(3)| BIT(2)| BIT(1));
+			conf.count_mode = (st | BIT(3) | BIT(2) | BIT(1));
 			break;
 		case base:
-			conf = (st|BIT(0));
+			conf.bcd = (st | BIT(0));
 			break;
 
 	}
 
-  timer_print_config(timer, field, conf) 
+  timer_print_config(timer, field, conf); 
+
   return 0;
 }
 
